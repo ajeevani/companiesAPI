0848c11e3d61e844ec123e842d8ea43b3629177e
Arman Jeevani <113207938+ajeevani@users.noreply.github.com>
2024-01-26 16:57:50 +0000

    Assignment 1

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
+/*********************************************************************************
+ * WEB422 – Assignment 1
+ *  I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+ * No part of this assignment has been copied manually or electronically from any other source
+ * (including web sites) or distributed to other students.
+ ** Name: Arman Jeevani Student ID: 158510180 Date: 24/01/2024
+ * Cyclic Link:https://crimson-abalone-yoke.cyclic.app/
+*/
+
 const express = require('express');
 const cors = require('cors');
 const CompaniesDB = require("./modules/companiesDB.js");
